6aab7874762d3075ea9d2db7bb6ae1a5a02a6032
Gene Hightower <gene@digilicious.com>
2023-04-05 14:54:45 -0700

    mess with the format

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@
 
 const world = "world";
 
-export function hello(who: string = world): string {
-  return `Hello ${who}!`;
+export function
+hello(who: string = world): string
+{
+    return `Hello ${who}!`;
 }
