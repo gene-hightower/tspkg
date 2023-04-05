dd5753e48c092f16258111ad0e41bd3fd974b13f
Gene Hightower <gene@digilicious.com>
2023-04-05 09:10:23 -0700

    all the boilerplate

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// tspkg - package to test CI and stuff
+
+// Copyright (C) 2023 Gene Hightower <gene@digilicious.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, version 3.
+
+const world = 'world';
+
+export function hello(who: string = world): string {
+  return `Hello ${who}! `;
+}
